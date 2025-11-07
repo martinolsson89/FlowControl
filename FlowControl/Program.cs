bf7d9360527ee2e3fbb2aec406ffd170baf53eab
Martin Olsson <martinolsson89@gmail.com>
2025-11-07 10:57:30 +0100

    Created menu and switch statement

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -5,7 +5,43 @@ namespace FlowControl
     {
         static void Main(string[] args)
         {
+            Run();
+        }
 
+        private static void Run()
+        {
+            while (true)
+            {
+                PrintMenu();
+                var choice = ReadInput("Välj ett alternativ: ");
+
+                switch (choice)
+                {
+                    case "0":
+                        Console.WriteLine("Avslutar...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Ogiltigt val.\n");
+                        break;
+                }
+
+            }
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine("==== Huvudmeny ====");
+            Console.WriteLine("0) Avsluta");
+            Console.WriteLine();
+        }
+
+        // Helper Method
+
+        private static string ReadInput(string prompt)
+        {
+            Console.Write(prompt);
+            return (Console.ReadLine() ?? string.Empty).Trim();
         }
     }
 }
